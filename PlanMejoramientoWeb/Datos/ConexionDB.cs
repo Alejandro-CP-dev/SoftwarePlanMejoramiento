b819b4a5332e041df6d8d950c870f74304b48da7
Jhon Cardenas <alejandrotareas029@gmail.com>
2026-05-26 22:09:33 -0500

    v0.0Configuracion de la cadena de conexion y el webconfig

---
+++ PlanMejoramientoWeb/Datos/ConexionDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PlanMejoramientoWeb.Datos
+{
+    public class ConexionDB
+    {
+        private static readonly string cadena = System.Configuration.ConfigurationManager.ConnectionStrings["ConexionDB"].ConnectionString;
+
+        public static SqlConnection MtAbrirConexion()
+        {
+            if (string.IsNullOrEmpty(cadena))
+            {
+                throw new Exception("Error en la configuracion de la conxecion");
+            }
+
+            return new SqlConnection(cadena);
+        }
+    }
+}
